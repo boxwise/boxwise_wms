dc23a4e8f5b3f470c9336f58e1c89072e83f7d27
HaGuesto <h.guertner@humanilog.org>
2018-12-15 16:09:20 +0100

    Print QR-codes of packages

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'website': "www.humanilog.org",
 
     'category': 'Uncategorized',
-    'version': '11.0.1.0.1',
+    'version': '11.0.1.0.5',
 
     'depends': [
 	    'stock', 'website'
@@ -18,10 +18,12 @@
     'data': [
         'data/product.category.csv',
         'data/product.template.csv',
-	'data/product.attribute.csv',
-	#'data/product.attribute.value.csv',
+	    'data/product.attribute.csv',
+	    #'data/product.attribute.value.csv',
         'views/edit_package.xml',
         'views/labeling.xml',
-        'data/res.partner.csv'
-    ],
+        'data/res.partner.csv',
+        #'views/generate_qr.xml',
+        'views/report_qr_codes.xml'
+    ]
 }
