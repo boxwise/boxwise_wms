1b29a583f0d5f85ed9e9b5bdb593232608abdda4
James Crowley <james.crowley@gmail.com>
2019-01-26 13:41:54 +0200

    Switch to lowercase naming convention for the Boxwise Odoo add-on. (#19)
    Odoo uses the folder name to determine the add-on namespace, so the current naming required
    the folder to be boxwise_WMS. Hopefully by making it lowercase there will be less issues
    when people clone this from the Git repository. But maybe that was just me!

--- controllers/main.py
+++ controllers/main.py
@@ -5,12 +5,12 @@ class LabelingController(http.Controller):
     @http.route('/boxwise/labeling/', auth='user')
     def index(self, **kw):
         packages = http.request.env['stock.quant.package']
-        return http.request.render('boxwise_WMS.labeling', {
+        return http.request.render('boxwise_wms.labeling', {
             'packages': packages.search([])
         })
 
     @http.route('/boxwise/labeling/<model("stock.quant.package"):package>/', auth='user')
     def edit_package(self, package):
-        return http.request.render('boxwise_WMS.edit_package', {
+        return http.request.render('boxwise_wms.edit_package', {
             'package': package
         })
