e03800020918c00dd4bcf0b16c3c6239004fbdee
DurkoMatko <nitramdurcek@gmail.com>
2019-02-09 10:53:35 +0100

    add: mass generation of packages (#9)

--- wizards/generate_qr_count_wizard.py
+++ wizards/generate_qr_count_wizard.py
@@ -5,22 +5,28 @@ import logging
 _logger = logging.getLogger(__name__)
 
 class GenerateQRCountWizard(models.TransientModel):
-    _name = 'boxwise.qr.generate.wizard'
+    _name = 'boxwise_wms.qr.generate.wizard'
     _description = 'Number of QR codes to be generated'
-    number = fields.Char(string='Number of labels', required=True)
-    create_uids = fields.Many2many('res.users', string='Created by')
-    make_big_labels = fields.Boolean('Make big labels including fields for box number and contents', default=False)
+    number = fields.Char(string='Number of labels', required=True, default=0)
 
     @api.multi
     def mass_generate(self):
         _logger.debug('Starting batch QR codes generation')
-        self.ensure_one()
-        vals = {}
+
+        packageModel = self.env['stock.quant.package']
+        number_of_packages = int(self.number)
+        createdPackages = []
+
+        for _ in range(number_of_packages):
+            createdPackages.append(packageModel.create({}))
+
+        return self.open_packages_list()
         
-        if not (self.number or self.create_uids):
-            raise exceptions.ValidationError('No data to update!')
-            _logger.debug('Mass generate failed')
-        if self.number:
-            vals['number'] = self.number
-        if self.create_uids:
-            vals['user_id'] = self.create_uids
+    def open_packages_list(self):
+        return {
+            'type':'ir.actions.act_window',
+            'res_model':'stock.quant.package',
+            'view_type':'list',
+            'view_mode':'list',
+            'target':'current',
+        }
