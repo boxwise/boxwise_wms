5a2cfbf1955987350ff55e633a7f885d3bbd47c6
HaGuesto <hans@boxwise.co>
2019-02-20 11:13:44 +0100

    Creating a base structure for frontend App

--- __manifest__.py
+++ __manifest__.py
@@ -12,21 +12,29 @@
     'category': 'Uncategorized',
     'version': '11.0.1.0.5',
 
-    'depends': [
-            'stock', 'website'
-    ],
+    'depends': ['stock', 'website_menu_by_user_status'],
     'data': [
         'data/product.category.csv',
         'data/product_category.xml',
         'data/product.attribute.csv',
         'data/product.attribute.value.csv',
         'data/product.template.csv',
-
+        'data/main_partner.xml',
+        'data/res.partner.csv',
+        'data/website_menu.xml',
+        'data/website_page.xml',
+        # 'data/website_redirect.xml',
         'views/packages.xml',
         'views/edit_package.xml',
-        'views/labeling.xml',
-        'data/res.partner.csv',
+        'views/find_package.xml',
+        'views/show_package.xml',
         'views/generate_qr.xml',
-        'views/report_qr_codes.xml'
-    ]
+        'views/report_qr_codes.xml',
+        'templates/layout.xml',
+        'templates/assets.xml',
+        # demo data
+        # 'demo/stock.move.csv',
+        # 'demo/stock.picking.csv'
+    ],
+
 }
