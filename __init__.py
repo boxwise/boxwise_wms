90d69f817cf4e193638ae549f4ed33a63033d079
James Crowley <james.crowley@gmail.com>
2019-02-25 19:06:13 +0200

    Rather than relying on XML to update and remove database entries, switch to python so we can fail gracefully when there is no action to take rather than generating loads of errors and noise.
    I had originally thought I could use the post_init_hook but this is only triggered on first install, not subsequent upgrades. Given we're lazy at the moment in terms of module versioning, using Odoo's support for migrations didn't seem to make sense - so instead I've done an ugly hack that abuses the ability to call functions from data files.
    
    I had to create a dummy model for this which I'm not too happy about (boxwise_config). If someone has a better way to do this later, then please knock yourselves out!

--- __init__.py
+++ __init__.py
@@ -3,3 +3,62 @@
 from . import controllers
 from . import wizards
 from . import sequences
+
+from odoo import api, SUPERUSER_ID, fields, models
+
+import wdb
+import logging
+
+_logger = logging.getLogger(__name__)
+
+def post_init_hook(cr, registry):
+    # this will only run once, when the module is
+    # first installed
+    # will need to use migrations for subsequent runs
+    _logger.info("post_init_hook for boxwise_wms")
+
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        run_on_first_install(env)
+
+        
+class boxwise_config(models.AbstractModel):
+    # hack to call this on every time our module loads (upgrade or not)
+    @api.model
+    def xml_init(self):
+        _logger.info("boxwise_config_xml_init for boxwise_wms")
+        run_on_every_install_or_upgrade(self.env)
+
+def run_on_first_install(env):
+    _logger.info("Running boxwise_wms steps: first install")
+    return
+
+def run_on_every_install_or_upgrade(env):
+    # really we should be using the migration features, that
+    # are applied for each version (see 'Addon updates and data migration' in Odoo cookbook)
+    # but this seems simpler for now
+    _logger.info("Running boxwise_wms steps: every install or upgrade")
+    #wdb.set_trace()
+    # these are created when we install the stock module
+    _delete_if_exists(env, "product", "product_category_1")
+    # these are created when we install the website module
+    # it does however mean we can't ever create something called this...
+    _delete_if_exists(env, "website", "homepage_page")
+    _delete_if_exists(env, "website", "contactus_page")
+    _delete_if_exists(env, "website", "aboutus_page")
+    _delete_if_exists(env, "website", "menu_homepage")
+    _delete_if_exists(env, "website", "menu_contactus")
+
+    _logger.info("Setting stock sequence to random_id")
+    seq = env.ref("stock.seq_quant_package")
+    if not seq.implementation == 'random_id':
+        seq.write({'implementation': 'random_id'})
+
+def _delete_if_exists(env, module_name, xml_id):
+    target_name = module_name + '.' + xml_id
+    item_to_delete = env.ref(module_name + '.' + xml_id, raise_if_not_found=False)
+    if (item_to_delete):
+        _logger.info("Deleting %s" % target_name)
+        item_to_delete.unlink()
+    else:
+        _logger.info("Skipping deletion of %s as it is not present" % target_name)
