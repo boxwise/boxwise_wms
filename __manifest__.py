0ce6c19b3af006db91109cc8dbc64776020971ae
James Crowley <james.crowley@gmail.com>
2019-02-25 12:12:20 +0200

    Initial import for Pampiraki specific data

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Boxwise Warehouse Management - Pampiraki Data",
+
+    'author': "Humanilog",
+    'website': "www.humanilog.org",
+
+    'category': 'Uncategorized',
+    'version': '1.0',
+
+    'depends': ['stock'],
+
+    'data': [
+        'data/product.category.csv',
+        'data/product_category.xml',
+        'data/product.attribute.csv',
+        'data/product.attribute.value.csv',
+        'data/product.template.csv',
+        'data/res.partner.csv',
+        # demo data
+        'demo/stock.move.csv',
+        'demo/stock.picking.csv'
+    ],
+}
