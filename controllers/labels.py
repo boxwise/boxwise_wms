6b7a08e0a73ddfe7f038deae66a1b59079cf5dc8
James Crowley <james.crowley@gmail.com>
2019-02-26 16:37:42 +0200

    Add the generate labels option to the home page

--- controllers/labels.py
+++ controllers/labels.py
@@ -15,3 +15,33 @@ class LabelController(http.Controller):
             return werkzeug.utils.redirect('/box/%s/edit' % slug(package))
         else:
             return werkzeug.utils.redirect('/box/%s' % slug(package))
+
+    @http.route('/labels/generate/', auth='user', methods=['POST'])
+    def generate(self, count):
+
+        package_model = http.request.env['stock.quant.package']
+        docids = []
+        for _ in range(int(count)):
+            docids.append(package_model.create({}).id)
+
+        return self._generate_qrcodes_report(docids)
+
+    @http.route('/labels/reprint/', auth='user', methods=['POST'])
+    def reprint(self):
+
+        packages = http.request.env['stock.quant.package'].search([])
+        docids = []
+        for pack in packages:
+            if not any(pack.move_line_ids):
+                docids.append(pack.id)
+
+        return self._generate_qrcodes_report(docids)
+
+    def _generate_qrcodes_report(self, docids):
+        report = http.request.env['ir.actions.report']._get_report_from_name('boxwise_wms.report_qr_codes_template')
+        context = dict(http.request.env.context)
+
+        pdf = report.with_context(context).render_qweb_pdf(docids)[0]
+
+        pdfhttpheaders = [('Content-Type', 'application/pdf'), ('Content-Length', len(pdf))]
+        return http.request.make_response(pdf, headers=pdfhttpheaders)
