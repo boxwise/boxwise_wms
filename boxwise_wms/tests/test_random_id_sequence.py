c47adc774ace9d339405c31d15e8b655bee3c0aa
DurkoMatko <nitramdurcek@gmail.com>
2019-04-03 01:23:38 +0200

    add: random_id sequence uniqueness test

--- boxwise_wms/tests/test_random_id_sequence.py
+++ boxwise_wms/tests/test_random_id_sequence.py
@@ -1,4 +1,5 @@
 from odoo.tests import common
+from odoo.exceptions import Warning as UserWarning
 import logging
 import re
 
@@ -14,7 +15,6 @@ class TestRandomIdSequence(common.TransactionCase):
 
     @common.post_install(True)
     def test_random_id_sequence(self):
-        current_company_id = self.env['res.company']._company_default_get().id
         sequence_model = self.env.ref("stock.seq_quant_package")
         if not sequence_model.implementation == 'random_id':
             sequence_model.write({'implementation': 'random_id'})
@@ -28,8 +28,8 @@ class TestRandomIdSequence(common.TransactionCase):
     def test_random_id_sequence_with_prefix_and_suffix(self):
         sequence_model = self.env.ref("stock.seq_quant_package")
         sequence_model.write({'implementation': 'random_id'})
-        sequence_model.prefix = "prefix-"
-        sequence_model.suffix = "-suffix"
+        sequence_model.write({'prefix': 'prefix-'})
+        sequence_model.write({'suffix': '-suffix'})
         generatedSequence = sequence_model.next_by_code('stock.quant.package')
 
         regexPattern = "prefix-\d{" + str(sequence_model.padding) + "}-suffix"
@@ -48,11 +48,30 @@ class TestRandomIdSequence(common.TransactionCase):
     @common.post_install(True)
     def test_random_id_sequence_length(self):
         sequence_model = self.env.ref("stock.seq_quant_package")
-        sequence_model.padding = 5
+        sequence_model.write({'implementation': 'random_id'})
+        sequence_model.write({'padding': 5})
         generatedSequence = sequence_model.next_by_code('stock.quant.package')
-        sequence_model.padding = 7
+        sequence_model.write({'padding': 7})
         generatedSequence2 = sequence_model.next_by_code('stock.quant.package')
         self.assertEqual(len(generatedSequence)<len(generatedSequence2), True)
 
+    @common.post_install(True)
+    def test_random_id_sequence_is_unique(self):
+        #creates 9 packages with IDs from 0 to 9 to ensure random ID with only one digit can't be generated anymore
+        package_model = self.env['stock.quant.package']
+        for i in range(10):
+            package_model.create({'name': str(i)})
+
+        #set lenght of sequence to 1 (enabling only IDs from 0 to 9, which are already taken)
+        sequence_model = self.env.ref("stock.seq_quant_package")
+        sequence_model.write({'implementation': 'random_id'})
+        sequence_model.write({'padding': 1})
+        try:
+            sequence_model.next_by_code('stock.quant.package')
+        #if the warning is published, one digit random_id sequence could not be generated (because all IDs are already taken)
+        except UserWarning:
+            self.assertTrue(True)
+            return
+        self.assertTrue(False)
 
         
